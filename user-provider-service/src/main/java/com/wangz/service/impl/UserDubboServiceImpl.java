32370d300e2b6d2e83e15d9d5a19bf91cef7ea51
wangzun <1125334796@qq.com>
2018-11-07 14:06:46 +0800

    初始化配置

--- user-provider-service/src/main/java/com/wangz/service/impl/UserDubboServiceImpl.java
+++ user-provider-service/src/main/java/com/wangz/service/impl/UserDubboServiceImpl.java
@@ -11,6 +11,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * @author wangzun
+ * @Desc user生产服务
+ * @version 2018/11/7 上午10:58
+ **/
 @Service(version = "1.0.0")
 public class UserDubboServiceImpl implements UserDubboService {
 
